a51b185e7471dc8730796e5fa516df9293e336d6
MuhammadOmarMuhdhar <167825709+MuhammadOmarMuhdhar@users.noreply.github.com>
2026-06-30 17:47:37 +0300

    added pagefind search

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://plus254.co.ke',
   base: '/',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap(), pagefind()]
 });
